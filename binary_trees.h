35d9c2d23464d846ee3ea6a98a618a47aa2172bd
starr-5 <studying12567@gmail.com>
2026-01-27 07:46:29 +0000

    Task 0 is completed

---
+++ binary_trees.h
@@ -0,0 +1,23 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child
+ * @right: Pointer to the right child
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H */ 
