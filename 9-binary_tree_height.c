b3de6b15636b902d6cf9d5df64e19fce233e2fa6
MNIGARR <nigarrmustafazada@gmail.com>
2026-01-30 11:57:42 +0000

    Done

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,18 +11,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
     size_t left_height = 0;
     size_t right_height = 0;
 
+    /* If it is leaf it will return 0 */
     if(tree == NULL){
         return (0);
     }
 
+    /* Calculate the height of the left side */
     if(tree->left){
         left_height = 1 + binary_tree_height(tree->left);
     }
 
+    /* Calculate the height of the right side */
     if(tree->right){
         left_height = 1 + binary_tree_height(tree->right);
     }
 
+    /* Return which part is the heighest */
     if(left_height > right_height){
         return (left_height);
     }
